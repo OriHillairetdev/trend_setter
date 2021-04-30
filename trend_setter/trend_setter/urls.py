7a687e04bea20f8e6e3e42a7ca5d26600eaca11a
OriHillairetdev <Wolfhilliaret@gmail.com>
2021-04-30 10:00:59 +0000

    Updated trendsetter settings

--- trend_setter/trend_setter/urls.py
+++ trend_setter/trend_setter/urls.py
@@ -1,4 +1,4 @@
-"""boutique_ado URL Configuration
+"""trend_setter URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,16 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
-    path('products/', include('products.urls')),
-    path('bag/', include('bag.urls')),
-    path('checkout/', include('checkout.urls')),
-    path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
