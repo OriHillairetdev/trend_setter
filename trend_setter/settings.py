e33168b1e6cb98170c13f89ac7beb94ed1ae0d54
OriHillairetdev <Wolfhilliaret@gmail.com>
2021-07-15 12:57:03 +0000

    removed secret key

--- trend_setter/settings.py
+++ trend_setter/settings.py
@@ -210,8 +210,10 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'oridev97@gmail.com'
 else:
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '587',
-    MAIL_USE_TLS = True,
-    MAIL_USERNAME = os.environ.get('EMAIL_USER'),
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
