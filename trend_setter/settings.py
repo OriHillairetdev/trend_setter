74c8aec058b1b596feb01e45165724b80fe8e2ca
OriHillairetdev <Wolfhilliaret@gmail.com>
2021-07-30 13:12:43 +0000

    changed src for header image

--- trend_setter/settings.py
+++ trend_setter/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'h%qme7bm@%dd&wql#g)wb&3^yso2bx%sfv*f0j$o77__!_ulcj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
