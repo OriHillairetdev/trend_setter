d1be9e724cc814ae92eac14053ae8533e8eb2622
OriHillairetdev <oridev97@gmail.com>
2023-01-10 14:08:40 +0000

    1

--- trend_setter/settings.py
+++ trend_setter/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'h%qme7bm@%dd&wql#g)wb&3^yso2bx%sfv*f0
 DEBUG = False
 
 ALLOWED_HOSTS = ["trend-setter.herokuapp.com", "localhost"]
-
- # Add Render.com URL to allowed hosts
- RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
  if RENDER_EXTERNAL_HOSTNAME:
    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 
 INSTALLED_APPS = [
