9991c5df006f4f2dc42703629b44ddaadf4a0bd3
OriHillairetdev <oridev97@gmail.com>
2023-01-10 14:20:14 +0000

    2

--- trend_setter/settings.py
+++ trend_setter/settings.py
@@ -29,8 +29,8 @@ DEBUG = False
 ALLOWED_HOSTS = ["trend-setter.herokuapp.com", "localhost"]
 # Add Render.com URL to allowed hosts
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
- if RENDER_EXTERNAL_HOSTNAME:
-   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
